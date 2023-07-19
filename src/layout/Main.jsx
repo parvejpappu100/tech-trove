c961d8a2a4c532303c4f1ff2870678f0db9c0f68
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-07-20 00:54:37 +0600

    added header info

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import { Outlet } from "react-router-dom";
+import Header from '../components/Header/Header';
 
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
