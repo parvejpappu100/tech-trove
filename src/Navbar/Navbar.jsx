0eb3cbd43e7afeeccb4e5fa6108c6de00ed7a04d
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-07-20 02:45:07 +0600

    added navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import "./Navbar.css"
+import { FaAlignLeft } from 'react-icons/fa';
 
 const Navbar = () => {
+
+    const navOptions = <>
+        <li>
+            <a>Home</a>
+        </li>
+        <li>
+            <a>Pages</a>
+        </li>
+        <li>
+            <a>Shop</a>
+        </li>
+        <li>
+            <a>Blog</a>
+        </li>
+        <li>
+            <a>Contact</a>
+        </li>
+    </>
+
     return (
-        <div>
-            <h1>This is navbar</h1>
+        <div className=''>
+            <div className="navbar lg:container mx-auto py-0 -mt-2">
+                <div className="navbar-start custom">
+                    <div className="dropdown ">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <FaAlignLeft className='text-2xl'></FaAlignLeft>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52 text-xl">
+                            {navOptions}
+                        </ul>
+                    </div>
+                    <div className='py-1 bg-[#113366]'>
+                        <select className=" select bg-[#113366]  text-white rounded-none w-full max-w-xs  focus:outline-none hidden lg:block">
+                            <option disabled selected>All Categories</option>
+                            <option>Svelte</option>
+                            <option>Vue</option>
+                            <option>React</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex custom ">
+                    <ul className="menu menu-horizontal px-1 text-xl ">
+                        {navOptions}
+                    </ul>
+                </div>
+                <div className="navbar-end ">
+                    <p className='font-bold hover:text-red-600 hidden lg:block'><span className='text-red-600'>Hotline : </span>+467247 - 94546</p>
+                    <div>
+                        <h5 className='text-2xl text-[#15407F] font-bold lg:hidden'>TechTrove</h5>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -3,7 +3,7 @@ import { FaCartArrowDown } from 'react-icons/fa';
 
 const SearchBar = () => {
     return (
-        <div className='bg-[#15407F] py-2 lg:py-5 text-white'>
+        <div className='bg-[#15407F] py-2 lg:py-5 text-white '>
             <div className='lg:container mx-auto flex flex-col lg:flex-row gap-5 lg:gap-0  justify-between items-center'>
                 <div>
                     <h5 className='text-4xl font-bold hidden lg:block'>TechTrove</h5>
@@ -19,7 +19,8 @@ const SearchBar = () => {
                         <input type="text" placeholder="Type here" className="input input-bordered w-full rounded-none text-black" />
                     </div>
                 </div>
-                <div className='text-3xl'>
+                <div className='text-3xl indicator'>
+                    <span className="indicator-item badge badge-secondary">1</span>
                     <button><FaCartArrowDown></FaCartArrowDown></button>
                 </div>
             </div>
