aa976e1c2dced0a0bf399d925641909cf55d9a6a
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-07-20 01:18:48 +0600

    added search bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import { FaCheck, FaLock } from "react-icons/fa";
+import HeaderInfo from './HeaderInfo';
+import SearchBar from './SearchBar';
 
 const Header = () => {
     return (
-        <div className='bg-[#113366] text-white py-2 '>
-            <div className='lg:container mx-auto flex flex-col md:flex-row md:px-4 lg:px-0 items-center justify-between '>
-                <div className='flex items-center gap-3 '>
-                    <FaCheck></FaCheck>
-                    <h4>Free shipping on all orders over 50$</h4>
-                </div>
-                <div className='flex items-center gap-3'>
-                    <select className="select bg-[#113366] w-full max-w-xs">
-                        <option disabled selected>Languages</option>
-                        <option>Svelte</option>
-                        <option>Vue</option>
-                        <option>React</option>
-                    </select>
-                    <span>|</span>
-                    <div className='flex items-center gap-3'>
-                        <FaLock></FaLock>
-                        <button>Login</button>
-                        <span>Or</span>
-                        <button>Register</button>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <HeaderInfo></HeaderInfo>
+            <SearchBar></SearchBar>
         </div>
     );
 };
---
+++ src/components/Header/SearchBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaCartArrowDown } from 'react-icons/fa';
+
+const SearchBar = () => {
+    return (
+        <div className='bg-[#15407F] py-2 lg:py-5 text-white'>
+            <div className='lg:container mx-auto flex flex-col lg:flex-row gap-5 lg:gap-0  justify-between items-center'>
+                <div>
+                    <h5 className='text-4xl font-bold hidden lg:block'>TechTrove</h5>
+                </div>
+                <div className='md:w-[600px] flex border-4 border-white rounded-xl'>
+                    <select className="select bg-[#15407F] w-full max-w-[140px]">
+                        <option disabled selected>Categories</option>
+                        <option>Svelte</option>
+                        <option>Vue</option>
+                        <option>React</option>
+                    </select>
+                    <div className='w-full'>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full rounded-none text-black" />
+                    </div>
+                </div>
+                <div className='text-3xl'>
+                    <button><FaCartArrowDown></FaCartArrowDown></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchBar;
