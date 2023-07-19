7892c7443459c89c83b4888139bb90fec6678b95
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-07-20 00:37:49 +0600

    added tailwind and daisyUI

---
+++ src/routes/Router.jsx
@@ -0,0 +1,19 @@
+import Main from "../layout/Main";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
+
+export default router;
