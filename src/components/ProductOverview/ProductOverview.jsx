75c851567f595f8dba66e4a111ba8d9bd3c87545
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-08-07 22:51:59 +0600

    added review

--- src/components/ProductOverview/ProductOverview.jsx
+++ src/components/ProductOverview/ProductOverview.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const ProductOverview = ({ product }) => {
     return (
@@ -26,7 +28,26 @@ const ProductOverview = ({ product }) => {
                     </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <h2 className='text-2xl my-8 font-semibold'>Customer Review</h2>
+                    <div className='mb-5'>
+                        <Rating style={{ maxWidth: 100 }} value="5" readOnly />
+                    </div>
+                    <hr className='w-full' />
+                    <div className='my-12'>
+                        {
+                            product?.reviews.map(review => <div key={review._id}>
+                                <div className='mb-16'>
+                                    <h3 className='text-xl font-semibold my-3'>{review.name}</h3>
+                                    <p>{review.date}</p>
+                                    <div className='my-5'>
+                                        <Rating style={{ maxWidth: 100 }} value={review.rating} readOnly />
+                                    </div>
+                                    <p>{review.review}</p>
+                                    <img className='my-5 w-[200px]' src={review?.img} alt="" />
+                                </div>
+                            </div>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
                     <h2>Any content 2</h2>
