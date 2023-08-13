ab6db20bdb92114192aa303687c8358c6bb3cdbb
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-08-13 21:32:28 +0600

    calculate total amount

--- src/components/MyCartProducts/MyCartProducts.jsx
+++ src/components/MyCartProducts/MyCartProducts.jsx
@@ -6,6 +6,7 @@ import Swal from 'sweetalert2';
 const MyCartProducts = ({ item, refetch }) => {
 
     const [isDisable, setIsDisable] = useState(true);
+    const [updating , setUpdating] = useState(false);
 
     const handleChangeQuantity = (event) => {
         console.log()
@@ -19,6 +20,7 @@ const MyCartProducts = ({ item, refetch }) => {
 
     const handleUpdatePrice = (event) => {
         event.preventDefault();
+        setUpdating(true)
         const quantity = parseInt(event.target.quantity.value);
         const updatedQuantity = { newQuantity: quantity }
         fetch(`http://localhost:5000/carts/${item._id}`, {
@@ -33,6 +35,7 @@ const MyCartProducts = ({ item, refetch }) => {
                 console.log(data)
                 refetch();
                 setIsDisable(true)
+                setUpdating(false)
 
             })
 
@@ -73,17 +76,17 @@ const MyCartProducts = ({ item, refetch }) => {
         <div>
             <div className='bg-white p-8 mb-5'>
                 <div className='flex flex-col md:flex-row gap-5 md:gap-0 justify-between'>
-                    <img className='w-[80px] h-[80px]' src={item.image} alt="" />
+                    <img className='w-[80px] h-[80px] border' src={item.image} alt="" />
                     <h4 className='font-semibold'>{item.name}</h4>
-                    <div className='flex items-center flex-col  gap-3'>
+                    <div className='flex md:items-center flex-col  gap-3'>
                         <p className='text-yellow-400 font-bold'>${item.price * item.productQuantity}</p>
                         <p className=' font-bold'>${item.price}</p>
                         <button><FaRegHeart></FaRegHeart></button>
                         <button onClick={handleDeleteProduct} className='text-red-500'><FaTrashAlt></FaTrashAlt></button>
                     </div>
-                    <form onSubmit={handleUpdatePrice} className='flex flex-col justify-between'>
+                    <form onSubmit={handleUpdatePrice} className='flex gap-5 flex-col justify-between'>
                         <input onChange={handleChangeQuantity} className='border bg-gray-200 text-center w-[100px]' type="number" name="quantity" defaultValue={item.productQuantity} id="" />
-                        <input disabled={isDisable} className='btn btn-xs normal-case bg-[#15407F] hover:bg-[#15407F] p-0 text-white font-semibold rounded-none' type="submit" value="Update Price" />
+                        <input disabled={isDisable} className='btn btn-xs normal-case bg-[#15407F] hover:bg-[#15407F] p-0 text-white font-semibold rounded-none w-24' type="submit" value={updating ? "Updating..." : "Update Price"} />
                     </form>
                 </div>
             </div>
--- src/pages/MyCart/MyCart.jsx
+++ src/pages/MyCart/MyCart.jsx
@@ -9,12 +9,23 @@ import { Link } from 'react-router-dom';
 const MyCart = () => {
 
     const [cart , refetch] = useCart();
+    console.log(cart)
+
+    let totalPrice = 0;
+    const shipping = totalPrice >= 50 ? 0 : totalPrice - (totalPrice * 10 / 100);
+
+    for(const product of cart){
+        const productTotal = product.price * product.productQuantity;
+        totalPrice += productTotal;
+    };
+
+    console.log(totalPrice)
 
     return (
         <div className='bg-[#F5F5F5]'>
             <PageTitle currentPage={"My Cart"}></PageTitle>
             <div className='lg:container mx-auto py-16'>
-                <div className='flex gap-10 items-start'>
+                <div className='flex gap-10 flex-col lg:flex-row items-start'>
                     <div className='max-w-4xl w-full'>
                         {
                            cart.length !== 0 ?  cart.map(item => <MyCartProducts key={item._id} refetch={refetch} item={item}></MyCartProducts>) : <div className='bg-[#15407F] py-3 text-white ps-5 font-semibold'>
@@ -22,27 +33,27 @@ const MyCart = () => {
                            </div>
                         }
                     </div>
-                    <div className='bg-white p-10 h-[400px] w-full max-w-[400px]'>
+                    <div className='bg-white p-10 h-[400px] w-full max-w-[400px] mx-auto'>
                         <h4 className='text-3xl font-semibold'>Cart Totals</h4>
                         <hr className='my-3' />
                         <div className='flex justify-between font-bold '>
                             <h3>Subtotal</h3>
-                            <p>$3454</p>
+                            <p>${totalPrice}</p>
                         </div>
                         <hr className='my-3' />
                         <div className='flex justify-between font-bold '>
                             <h3>Shipping</h3>
-                            <p>$34</p>
+                            <p>${shipping}</p>
                         </div>
                         <hr className='my-3' />
                         <div className='flex justify-between font-bold '>
                             <h3>Total</h3>
-                            <p>$3488</p>
+                            <p>${totalPrice + shipping}</p>
                         </div>
                         <hr className='my-3' />
                         <div className='flex justify-between font-bold '>
                             <h3>Payable Total</h3>
-                            <p>$3488</p>
+                            <p>${totalPrice + shipping}</p>
                         </div>
                         <hr className='my-3' />
                         <button className='btn w-full bg-[#113366] hover:bg-[#292929] text-white rounded-none font-semibold'>Proceed To Checkout</button>
