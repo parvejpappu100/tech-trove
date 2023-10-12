e84f12ad8571d53d4f970f60a805d6bb5b7aaea4
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-10-13 02:05:41 +0600

    saved product after clicking my cart page save btn

--- src/components/MyCartProducts/MyCartProducts.jsx
+++ src/components/MyCartProducts/MyCartProducts.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { FaRegHeart, FaTrashAlt } from 'react-icons/fa';
+import { FaHeart, FaRegHeart, FaTrashAlt } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
+import useAuth from '../../hooks/useAuth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import useSaved from '../../hooks/useSaved';
 
 
 const MyCartProducts = ({ item, refetch, setDisable }) => {
@@ -10,7 +13,12 @@ const MyCartProducts = ({ item, refetch, setDisable }) => {
     const [updating, setUpdating] = useState(false);
     const [axiosSecure] = useAxiosSecure();
     const [error, setError] = useState(null);
-    
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [saved, refetchSaved] = useSaved();
+    const savedProduct = saved.find(sp => sp.productId == item.productId);
+
     const productCurrentQuantity = item.availability;
     const number = parseInt(productCurrentQuantity?.split('(')[1], 10);
 
@@ -56,6 +64,40 @@ const MyCartProducts = ({ item, refetch, setDisable }) => {
 
     };
 
+    const handleSavedProduct = (item) => {
+        if (user && user.email) {
+            const savedItem = { productId: item.productId, productQuantity: item.productQuantity, name: item.name, image: item.image, email: user.email, price: item.price, availability: item.availability }
+            axiosSecure.post("/saved", savedItem)
+                .then(data => {
+                    if (data.data.insertedId) {
+                        refetchSaved();
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Product Saved Wishlist !',
+                            showConfirmButton: false,
+                            timer: 1000
+                        })
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: 'You have to Login first!',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Go to login'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate("/login", { state: { from: location } })
+                }
+            })
+        }
+    }
+
     const handleDeleteProduct = () => {
         Swal.fire({
             title: 'Are you sure?',
@@ -94,8 +136,12 @@ const MyCartProducts = ({ item, refetch, setDisable }) => {
                     <div className='flex md:items-center flex-col  gap-3'>
                         <p className='text-yellow-400 font-bold'>${item.price * item.productQuantity}</p>
                         <p className=' font-bold'>${item.price} x ${item.productQuantity}</p>
-                        <button><FaRegHeart></FaRegHeart></button>
-                        <button onClick={handleDeleteProduct} className='text-red-500'><FaTrashAlt></FaTrashAlt></button>
+                        <div>
+                            <button disabled={savedProduct} onClick={() => handleSavedProduct(item)} className='bg-white p-2 rounded-full tooltip' data-tip={savedProduct ? "Saved" : "Saved Product"}>
+                                {savedProduct ? <FaHeart className='text-pink-500'></FaHeart> : <FaRegHeart></FaRegHeart>}
+                            </button>
+                        </div>
+                        <button onClick={handleDeleteProduct} className='text-red-500 tooltip tooltip-right' data-tip="Delete"><FaTrashAlt></FaTrashAlt></button>
                     </div>
                     <form onSubmit={handleUpdatePrice} className='flex gap-5 flex-col justify-between'>
                         <input onChange={handleChangeQuantity} className='border bg-gray-200 text-center w-[100px]' type="number" name="quantity" defaultValue={item.productQuantity} id="" />
