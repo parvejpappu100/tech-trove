cb431b54d10dcec5f220feb83b600b491aa1ce2c
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-10-13 01:12:03 +0600

    delete saved cart product

--- src/components/SavedProduct/SavedProduct.jsx
+++ src/components/SavedProduct/SavedProduct.jsx
@@ -1,22 +1,54 @@
 import React from 'react';
 import { FaShoppingCart, FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const SavedProduct = ({ refetchSaved, item }) => {
 
-    console.log(item)
+    const [axiosSecure] = useAxiosSecure();
+
+    const handleAddToCart = (item) => {
+        
+    }
+
+    const handleDeleteItem = (item) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/saved-delete/${item._id}`)
+                    .then(data => {
+                        if (data.data.deletedCount > 0) {
+                            refetchSaved();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
 
     return (
         <div className='bg-white p-5 border'>
             <img className='h-72' src={item.image} alt="" />
             <h3 className='my-2 text-xl font-semibold'>{item.name}</h3>
-            <h5 className='text-xl font-semibold'>Available Quantity : {item.availability}</h5>
+            <h5 className='text-xl font-semibold'>{item.availability}</h5>
             <p className='my-2 text-xl font-semibold'>Price : ${item.price}</p>
             <div className='flex justify-between'>
-                <button className='btn'>
+                <button onClick={() => handleAddToCart(item)} className='btn'>
                     <FaShoppingCart></FaShoppingCart>
                     <span>Add To Cart</span>
                 </button>
-                <button className='btn'>
+                <button onClick={() => handleDeleteItem(item)} className='btn'>
                     <FaTrashAlt className='text-red-500'></FaTrashAlt>
                     <span>Delete</span>
                 </button>
